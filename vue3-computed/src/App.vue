b845f7131fa3500f9a063d48a9e7d5c0971fc87f
heee1 <p1xell@kakao.com>
2023-07-31 19:07:07 +0900

    230731_1907

--- vue3-computed/src/App.vue
+++ vue3-computed/src/App.vue
@@ -1,12 +1,6 @@
 <template>
-  <!-- v-bind:class == :class -->
-  <!-- 클릭하면 activate실행 -->
-  <!-- activate는 isActive를 true로 만들어줌 -->
-  <!-- isActive가 true면 active: true가 되니 스타일 적용 -->
-  <h1
-    :class="{active: isActive}"
-    @click="activate">
-    Hello?!({{ isActive }})
+  <h1 @click="activate">
+    Hello?!
   </h1>
 </template>
 
@@ -24,11 +18,3 @@
     }
   }
 </script>
-
-<style scoped>
-  /* scoped : 현재파일(App.vue)에서만 적용되는 스타일 */
-  .active{
-    color: red;
-    font-weight: bold;
-  }
-</style>
