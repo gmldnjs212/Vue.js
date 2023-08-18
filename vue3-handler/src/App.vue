91907b7135a4bbaa502cf5b916565f6a330782b4
heee1 <p1xell@kakao.com>
2023-08-18 19:19:13 +0900

    230818_1919

--- vue3-handler/src/App.vue
+++ vue3-handler/src/App.vue
@@ -1,30 +1,14 @@
 <template>
-  <button @click="message = 'Good?'">
-    Click!
-  </button>
-  <h1>APP: {{ message }}</h1>
-  <Parent />
+  <h1 ref="hello">
+    Hello World!
+  </h1>
 </template>
 
 <script>
-import Parent from '~/components/Parent'
-import { computed } from 'vue'
-
 export default{
-  components:{
-    Parent
-  },
-  data(){
-    return{
-      message: 'Hello World!'
-    }
-  },
-  provide(){
-    return{
-      msg: computed(()=>{
-        return this.message
-      })
-    }
+  mounted(){
+    console.log(this.$refs.hello);
+    console.log(this.$refs.hello.textContent);
   }
 }
 </script>
