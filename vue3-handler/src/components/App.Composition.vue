352db14ce7a983226f4bbd6413e35bb4885725e8
heee1 <p1xell@kakao.com>
2023-08-25 21:10:24 +0900

    230825_2110

--- vue3-handler/src/components/App.Composition.vue
+++ vue3-handler/src/components/App.Composition.vue
@@ -1,8 +1,49 @@
 <template>
-  <h1 @click="incresae">
-    {{ count }}
+  <h1 @click="increase">
+    {{ count }} / {{ doubleCount }}
   </h1>
   <h1 @click="changeMessage">
-    {{ message }}
+    {{ message }} / {{ reversedMessage }}
   </h1>
 </template>
+
+<script>
+import { ref, computed, watch, onMounted } from 'vue'
+
+export default{
+  setup(){
+    let count = ref(0)
+    const doubleCount = computed(()=>{
+      return count.value*2
+    })
+    function increase(){
+      count.value += 1
+    }
+    
+    let message = ref('Hello World')
+    const reversedMessage = computed(()=>{
+      return message.value.split('').reverse().join('')
+    })
+    watch(message, newValue=>{
+      console.log(newValue)
+    })
+    function changeMessage(){
+      message.value = 'Good?!'
+    }
+    console.log(message.value)
+  
+    onMounted(()=>{
+      console.log(count.value)
+    })
+
+    return{
+      count,
+      doubleCount,
+      increase,
+      message,
+      reversedMessage,
+      changeMessage
+    }
+  }
+}
+</script>
