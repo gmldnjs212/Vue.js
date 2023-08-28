2df147d77adee25fd39cd3a34eb82e5500ef8c1b
heee1 <p1xell@kakao.com>
2023-08-28 17:16:35 +0900

    230828_1716

--- vue3-handler/src/App.vue
+++ vue3-handler/src/App.vue
@@ -2,10 +2,10 @@
 
 <template>
   <h1 @click="increase">
-    {{ count }}
+    {{ count }} / {{ doubleCount }}
   </h1>
   <h1 @click="changeMessage">
-    {{ message }}
+    {{ message }} // {{ reversedMessage }}
   </h1>
 </template>
 
@@ -17,12 +17,21 @@ export default{
       message: "Hello World!"
     }
   },
+  // computed : 계산된 데이터
   computed:{
     doubleCount(){
-      return this.count
+      return this.count * 2
     },
     reversedMessage(){
       return this.message.split('').reverse().join('')
+    }
+  },
+  // watch : 명시한 데이터가 변경되면 실행
+  watch:{
+    // 메시지가 변경되면 새로운 메시지를 받아와서
+    // 콘솔에 새로운 메시지를 로그로 찍음
+    message(newValue){
+      console.log(newValue);
     }
   },
   methods:{
--- vue3-handler/src/components/App.Composition.vue
+++ vue3-handler/src/components/App.Composition.vue
@@ -10,24 +10,35 @@
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
 
 export default{
   setup(){
-    let count = ref(0)
+    const count = ref(0)
+    const doubleCount = computed(() => {
+      return count.value * 2
+    })
     function increase(){
       count.value += 1
     }
     
-    let message = ref('Hello World!')
+    const message = ref('Hello World!')
+    const reversedMessage = computed(() => {
+      return message.value.split('').reverse().join('')
+    })
+    watch(message, newValue =>{
+      console.log(newValue);
+    })
     function changeMessage(){
       message.value = 'Good?!'
     }
 
     return{
       count,
-      message,
+      doubleCount,
       increase,
+      message,
+      reversedMessage,
       changeMessage
     }
   }
